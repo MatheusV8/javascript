fe9e43d1e24e3e51de1335e1724c14be80098cf7
DanieleAnjos <daniele.lims02@gmail.com>
2023-06-01 10:38:46 -0300

    ...

---
+++ aula011/ex008.js
@@ -0,0 +1,6 @@
+var vel = 12
+console.log( `A velocidade do seu carro é ${vel}Km/h`)
+if (vel > 60) { //condição simpes!
+    console.log('você ultrapassou a velocidade permitia, MULTADO')
+}
+console.log(`Dirija sempre usando cinto de segurança`)
